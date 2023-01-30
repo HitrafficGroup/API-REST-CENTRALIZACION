7066d6c050cccd8cf347aec076d70a9842e2fb5a
david <david.diaz190799@gmail.com>
2023-01-30 17:08:31 -0500

    se crea el repositorio

---
+++ api/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    #Operaciones de lectura
+    path('listarIps', ListarIpsControlador.as_view()),
+    path('restGetTimeControlador',restGetTimeControlador.as_view()),
+    path('restGetGruposControlador',restGetGruposControlador.as_view()),
+    path('restGetFasesControlador',restGetFasesControlador.as_view()),
+    path('restGetPlanesControlador',restGetPlanesControlador.as_view()),
+    path('restGetOtrosParamControlador',restGetOtrosParamControlador.as_view()),
+    path('restGetHorariosControlador',restGetHorariosControlador.as_view()),
+    path('restGetDiasEspecialesControlador',restGetDiasEspecialesControlador.as_view()),
+    path('restGetConflictoVerdesControlador',restGetConflictoVerdesControlador.as_view()),
+    path('restGetEntradasControlador',restGetEntradasControlador.as_view()),
+    path('restGetRegistrosControlador',restGetRegistrosControlador.as_view()),
+    path('restGetVersionFirmware',restGetVersionFirmware.as_view()),
+    #Operaciones de Escritura
+    path('restSetTimeControlador',restSetTimeControlador.as_view()),
+    path('restSetGruposControlador',restSetGruposControlador.as_view()),
+    path('restSetConflictoVerdesControlador',restSetConflictoVerdesControlador.as_view()),
+    path('restSetFasesControlador',restSetFasesControlador.as_view()),
+    path('restSetPlanesControlador',restSetPlanesControlador.as_view()),
+    path('restSetOtrosParamControlador',restSetOtrosParamControlador.as_view()),
+    path('restSetHorariosControlador',restSetHorariosControlador.as_view()),
+    path('restSetDiasEspecialesControlador',restSetDiasEspecialesControlador.as_view()),    
+]
